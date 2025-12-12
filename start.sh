a3cdc95345e94eeb491071d8d44512ae52293b0d
Hesham Ahmed <heshmahmed146@gmail.com>
2025-12-12 16:53:09 +0200

    upload tunnel file

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# تثبيت cloudflared
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+sudo dpkg -i cloudflared-linux-amd64.deb
+
+# تشغيل Tunnel يمرر كل الطلبات إلى Backend
+cloudflared tunnel run my-tunnel --url http://mepboq.runasp.net
